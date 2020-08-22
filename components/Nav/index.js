dbdd1d1b8950e078ce25f618adfcbd6f15254068
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-08-21 23:14:57 -0300

    add nav for mobile

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { Drawer } from 'antd';
 
 import {
   InstagramOutlined,
@@ -12,7 +13,55 @@ import {
 
 import { Wrapper } from './styles';
 
+const NavItems = () => {
+  return (
+    <>
+      <ul className="nav-desktop">
+        <li><Link href="/category/leia"><a>Leia</a></Link></li>
+        <li><Link href="/shows"><a>Assista</a></Link></li>
+        <li><Link href="/category/ouca"><a>Ouça</a></Link></li>
+        <li>Fale</li>
+        {/*
+        <li>Compre</li>
+        */}
+        <li>
+          <SearchOutlined style={{ color: '#fff', marginLeft: '1rem' }} />
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="http://instagram.com/tudopeleja" target="_blank">
+            <InstagramOutlined />
+          </a>
+        </li>
+        <li>
+          <a href="http://facebook.com/tudopeleja" target="_blank">
+            <FacebookOutlined />
+          </a>
+        </li>
+        <li>
+          <a href="http://youtube.com/peleja" target="_blank">
+            <YoutubeOutlined />
+          </a>
+        </li>
+        <li>
+          <a href="http://twitter.com/tudopeleja" target="_blank">
+            <TwitterOutlined />
+          </a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
 export default function Nav() {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <Wrapper>
       <div>
@@ -22,45 +71,22 @@ export default function Nav() {
           </a>
         </Link>
         <aside>
-          <ul className="nav-desktop">
-            <li><Link href="/category/leia"><a>Leia</a></Link></li>
-            <li><Link href="/shows"><a>Assista</a></Link></li>
-            <li><Link href="/category/ouca"><a>Ouça</a></Link></li>
-            <li>Fale</li>
-            {/*
-            <li>Compre</li>
-            */}
-            <li>
-              <SearchOutlined style={{ color: '#fff', marginLeft: '1rem' }} />
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="http://instagram.com/tudopeleja" target="_blank">
-                <InstagramOutlined />
-              </a>
-            </li>
-            <li>
-              <a href="http://facebook.com/tudopeleja" target="_blank">
-                <FacebookOutlined />
-              </a>
-            </li>
-            <li>
-              <a href="http://youtube.com/peleja" target="_blank">
-                <YoutubeOutlined />
-              </a>
-            </li>
-            <li>
-              <a href="http://twitter.com/tudopeleja" target="_blank">
-                <TwitterOutlined />
-              </a>
-            </li>
-          </ul>
+          <NavItems />
           <div>
-            <MenuOutlined style={{ color: '#fff', fontSize: '18px' }} />
+            <MenuOutlined onClick={showDrawer} style={{ color: '#fff', fontSize: '18px' }} />
           </div>
         </aside>
       </div>
+      <Drawer
+        title="PELEJA"
+        className="nav-drawer"
+        placement="right"
+        closable={true}
+        onClose={onClose}
+        visible={visible}
+      >
+        <NavItems />
+      </Drawer>
     </Wrapper>
   );
 }

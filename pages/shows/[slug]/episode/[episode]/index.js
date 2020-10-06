097b0526de65b871d232dc8c0190b2cab8282501
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-10-06 20:54:42 -0300

    add Twitter Open Graph tags to episode page

--- pages/shows/[slug]/episode/[episode]/index.js
+++ pages/shows/[slug]/episode/[episode]/index.js
@@ -14,9 +14,13 @@ const EpisodeDetails = ({ episode, articles }) => {
         <meta property="og:site_name" content={SITE_NAME} />
         <meta property="og:title" content={`${episode.fields.title} | ${episode.fields.show.fields.title}`} />
         <meta property="og:description" content={episode.fields.shortDescription} />
-        <meta property="og:image" content={episode.fields.featuredImage.fields.file.url} />
+        <meta property="og:image" content={`https:${episode.fields.featuredImage.fields.file.url}`} />
         <meta property="og:url" content={`${SITE_URL}${router.asPath}`} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={SITE_NAME} />
+        <meta name="twitter:title" content={`${episode.fields.title} | ${SITE_NAME}`} />
+        <meta name="twitter:description" content={episode.fields.shortDescription} />
+        <meta name="twitter:image" content={`https:${episode.fields.featuredImage.fields.file.url}`} />
+        <meta name="twitter:card" content="summary_large_image"/>
       </Head>
       <Episode episode={episode} articles={articles} />
     </>

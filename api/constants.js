c75509c3fcdd9fea3a5b4fe33bd840004aa74a36
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-10-06 21:16:25 -0300

    update SITE_IMAGE

--- api/constants.js
+++ api/constants.js
@@ -1,7 +1,7 @@
 export const API_URL = 'https://cdn.contentful.com';
 export const SITE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://pelejamedia.com';
 export const SITE_NAME = 'PELEJA';
-export const SITE_IMAGE = 'https://pbs.twimg.com/profile_images/848947510525779969/2KmIZTbs_400x400.jpg';
+export const SITE_IMAGE = 'https://images.ctfassets.net/lgt1coqdvs74/7bWtx03QiRyPip5dKAx433/62cc57ed1e63534eda02dc09c6d524d8/logo-peleja.jpeg';
 export const SITE_DESCRIPTION = 'Conteúdos diários sobre o futebol pouco visto nas mídias em geral.';
 export const SPACE_ID = process.env.spaceId;
 export const ACCESS_TOKEN = process.env.accessToken;
